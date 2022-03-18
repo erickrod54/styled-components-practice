7c8e2ad6a825e363a0bc5c2520b33fbad1e60458
Erick Rodriguez <erickrod54@gmail.com>
2022-03-18 16:54:20 -0400

    Style Components app version 2 - Features: Copying padding from title and btn div to hello world div, Personalizing a style for hello wolrd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react'
-/**Style Components app version 1 - Features:
- *        --> Inline Styles 
- *        --> Global Stylesheet 
+/**Style Components app version 2 - Features:
+ *        --> Copying padding from the title and btn
+ *           'div' to hello world 'div'
+ *        -->Personalizing a style for hello 
+ *           world
  * 
- * Note: the Global Stylesheet  is all this styles are going to 
- * be built on index.css, -index contains 'App', and applied his 
- * stylesheet to 'App'
+ * Note: the implementations on this version will point
+ * the major flaws of 'Inline Styles' and 'Global 
+ * Stylesheet'
+ * 
+ * To personalize a style for example the 'title' also
+ * comes flaws due to complexity as child stylesheets
  */
 function App() {
   return (
     <>
-    {/**this is the inline style for a div that contain
-     * the title and a button*/}
     <div style={{padding:'2rem', backgroundColor:'#b11226'}}>
-      {/**i implement the 'title' style*/}
       <h2 className='title'>Styled Components</h2>
-      {/**i implement the 'btn' style*/}
       <button className='btn'>click me</button> 
+      {/**here i copy the title and btn div padding */}
+      <div style={{padding:'2rem'}}>
+      {/**here i make a style for 'hello world' */}
+        <h2 className='section-title'>hello world</h2>
+      </div>
     </div>
     </>
   );
