135edb40cdd5436aa34bd52ffdccb5183f76c116
Erick Rodriguez <erickrod54@gmail.com>
2022-03-20 05:53:43 -0400

    Style Components app version 3 - App js file - Features: Building Style Component for BasicTitle

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React from 'react'
-/**Style Components app version 2 - Features:
- *        --> Copying padding from the title and btn
- *           'div' to hello world 'div'
- *        -->Personalizing a style for hello 
- *           world
+import styled from 'styled-components';
+
+/**Style Components app version 3 - App js file - Features:
+ *        --> Building Style Component for 'BasicTitle'
  * 
- * Note: the implementations on this version will point
- * the major flaws of 'Inline Styles' and 'Global 
- * Stylesheet'
- * 
- * To personalize a style for example the 'title' also
- * comes flaws due to complexity as child stylesheets
+ * Note: to build style-components must be previously installed
+ * 'styled-components' library and imported styled from styled
+ * components, and to track bugs 'vscode-styled-components'
+ * extension
  */
+
+/**here i buil 'BasicTitle' Component */
+/**this kind of sintax is called tagged template string
+ * reference: freecodecamp article*/
+const BasicTitle = styled.h1`
+  text-align: center;
+  text-transform: capitalize;
+`
+
 function App() {
   return (
     <>
     <div style={{padding:'2rem', backgroundColor:'#b11226'}}>
-      <h2 className='title'>Styled Components</h2>
-      <button className='btn'>click me</button> 
-      {/**here i copy the title and btn div padding */}
-      <div style={{padding:'2rem'}}>
-      {/**here i make a style for 'hello world' */}
-        <h2 className='section-title'>hello world</h2>
-      </div>
+      {/**here i apply the Style Component to the title */}
+
+      {/**Inspecting with the browser will show the h1*/}
+     <BasicTitle>Styled Component</BasicTitle>
+     <BasicTitle>Styled Component</BasicTitle>
+     <BasicTitle>Styled Component</BasicTitle>
+     <BasicTitle>Styled Component</BasicTitle>
+       <button className='btn'>click me</button> 
+      
     </div>
     </>
   );
