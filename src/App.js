6298d0e3e3fc31cf8c23de0d3042dd228ef32185
Erick Rodriguez <erickrod54@gmail.com>
2022-03-25 18:41:59 -0400

    Style Components app version 5 - App js file - Features: Importing and Placing HipsterButton

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import BasicTitle from './components/title.styles';
-import { DefaultButton } from './components/button.styles';
+import { DefaultButton, HipsterButton } from './components/button.styles';
 
-/**Style Components app version 4 - App js file - Features:
- *        --> Building Style Component for 'DefaultButton'.
- *        --> Restructuring directory to separate style 
- *            components its own file.
- *        --> Importing Style Components.
- *        --> Customizing 'BasicTitle' implementing 'props' 
+/**Style Components app version 5 - App js file - Features:
+ *        --> Importing and Placing 'HipsterButton'
  * 
- * Note: Separating the styles components is the good practice
- * so they can be organized, to code will be more readable and
- * and can be easily modified
+ * Note: 'HipsterButton' is an extended styled 'React Component'
  */
 
 
@@ -26,7 +20,9 @@ function App() {
      {/**this is the title i'm going to customize*/}
      {/**i call the prop 'customPurple' */}
      <BasicTitle customPurple>Styled Component</BasicTitle> 
-     <DefaultButton>click me</DefaultButton>
+     <DefaultButton>click me </DefaultButton>
+     {/**Here i set 'HipsterButton' */}
+     <HipsterButton>Hipster Click </HipsterButton>
     </div>
     </>
   );
