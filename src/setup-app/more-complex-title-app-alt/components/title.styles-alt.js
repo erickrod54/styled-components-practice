3211e323c32c28ab962db50c1c8c65fec5deeecd
Erick Rodriguez <erickrod54@gmail.com>
2022-03-29 20:41:11 -0400

    More Complex Styled app version 3 - title.styles sheet Component - Features: (Alternative for version 2): Taking out Wrapper to be use on morecomplextitle-app-alt.jsx to extend MoreComplexTitle Component

---
+++ src/setup-app/more-complex-title-app-alt/components/title.styles-alt.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+/**More Complex Styled app version  3 - 'title.styles' sheet 
+ * Component - Features: (Alternative for version 2)
+ * 
+ *        --> Taking out 'Wrapper' to be use on 
+ *            'morecomplextitle-app-alt.jsx' to 
+ *             extend 'MoreComplexTitle' Component
+ * 
+ * Note: The functionality stays the same, what changes is
+ * the syntax implementation.
+ */
+
+
+export const WrapperRandom = styled.div`
+    .underline{
+        width: 5rem;
+        height: 0.5rem;
+        background: red;
+        margin: 4rem auto;
+    }
+`
