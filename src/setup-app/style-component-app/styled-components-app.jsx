bb18c95c767ad4a0f05c57c2caa101c296acafde
Erick Rodriguez <erickrod54@gmail.com>
2022-03-30 20:18:53 -0400

    Intalling packages MUI

---
+++ src/setup-app/style-component-app/styled-components-app.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import  BasicTitle  from './components/title.styles'
+import { DefaultButton } from './components/button.styles'
+import { HipsterButton } from "./components/button.styles";
+
+/**Style Components app version  6 - App js file - Features:
+ *        --> Updating app to own directory
+ *        --> Importing Styled Components 
+ * 
+ * Note: This update is made to add styled app easier by mounting
+ * them in App js
+ */
+
+const StyledComponentApp = () => {
+    return(
+    <>
+    <div style={{padding:'2rem', backgroundColor:'#b11226'}}>
+
+        {/**Inspecting with the browser will show the h1*/}
+        <BasicTitle>Styled Component</BasicTitle> 
+        {/**this is the title i'm going to customize*/}
+        {/**i call the prop 'customPurple' */}
+        <BasicTitle customPurple>Styled Component</BasicTitle> 
+        <DefaultButton>click me </DefaultButton>
+        {/**Here i set 'HipsterButton' */}
+        <HipsterButton>Hipster Click </HipsterButton>
+    </div>
+    </>
+    )
+
+}
+
+export default StyledComponentApp;
