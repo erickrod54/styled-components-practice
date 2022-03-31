803ab07360d997e394ed439cf1f7e159481a5674
Erick Rodriguez <erickrod54@gmail.com>
2022-03-31 17:03:07 -0400

    Style Components app version 7 - title.styles Component - Features: Changing export

--- src/setup-app/style-component-app/components/title.styles.js
+++ src/setup-app/style-component-app/components/title.styles.js
@@ -1,22 +1,20 @@
 import styled from 'styled-components';
-/**Style Components app version 5 - 'title.styles' Component - 
+/**Style Components app version 7 - 'title.styles' Component - 
  * Features:
  * 
- *        --> Accessing props by destructuring. 
+ *        --> Changing 'export'. 
  * 
  * Note: A good practice is to keep on this file the different
  * titles that i need  for the app in order to track them
  */
 
-const BasicTitle = styled.h1`
+/**here i change the export from implicit to explicit */
+export const BasicTitle = styled.h1`
   text-align: center;
   text-transform: capitalize;
   /**Here i only destructure it with the exact name and accesed*/
   //color:${({ customPurple }) => customPurple && '#e2bfd2'}
-
   /**Here i destructure it the same way but i set a ternary operator
   * to condition the style for element who posses or not CustomPurple*/
   color:${({ customPurple }) => customPurple ? '#e2bfd2' : '#9e934d'}
 `
-
-export default BasicTitle
