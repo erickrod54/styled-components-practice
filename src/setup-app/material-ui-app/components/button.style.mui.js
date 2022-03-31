15da09a325fa3b5703cc5b6f258a6fca346455f2
Erick Rodriguez <erickrod54@gmail.com>
2022-03-30 20:14:29 -0400

    Style Components app version 6 - button.style.mui sheet - Features: Bulding the extended Button StyleBtn

---
+++ src/setup-app/material-ui-app/components/button.style.mui.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { Button } from "@mui/material";
+
+/**Style Components app version  6 - button.style.mui sheet - 
+ * Features:
+ * 
+ *        --> Building the extended 'Button' 
+ * 
+ * Note: MUI stands for material ui - wonderfull material 
+ * components that i can integrate with react :)
+ */
+
+/**here i extend the original 'Button' */
+export const StyledBtn = styled(Button)`
+    text-transform: capitalize;
+`
