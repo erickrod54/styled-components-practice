bf1e8ce567f39817e7322dff1cd0e1b12efc3939
Erick Rodriguez <erickrod54@gmail.com>
2022-04-01 16:35:47 -0400

    More Complex Styled app version 6 - utils js - Features:(Alternative approach to build Global Variables using :root element) Building a Global Variable to keep and distribute a color

---
+++ src/utils.js
@@ -0,0 +1,18 @@
+/**More Complex Styled app version  6 - 'utils js' - Features:
+ * (Alternative approach to build Global Variables)
+ * 
+ *        --> Building a 'Global Variable' to keep and distribute
+ *            a color.
+ * 
+ * Note: this second approach dies the same that targeting ':root'
+ * element, this time i'll use this 'utils' file to keep the 
+ * styles
+ */
+
+/**this will be the Style component that will keep the 
+ * 'Global Variable' -this case 'colors'-
+ */
+export const colors = {
+    primary: '#e5d0ff'
+}
+
