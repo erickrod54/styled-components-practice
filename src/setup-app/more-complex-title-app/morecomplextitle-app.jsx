19da503785c06a29554ba90e441b0ecac072e621
Erick Rodriguez <erickrod54@gmail.com>
2022-04-01 16:46:31 -0400

    More Complex app version 7 - MoreComplexTitle Component - Features: Applying a box class style defined in Wrapper Component

--- src/setup-app/more-complex-title-app/morecomplextitle-app.jsx
+++ src/setup-app/more-complex-title-app/morecomplextitle-app.jsx
@@ -2,24 +2,15 @@ import React from "react";
 import { Wrapper } from "./components/title.styles";
 import RandomUnderline from "./random-underline.component";
 
-/**More Complex Styled app version  4 - 'MoreComplexTitle' 
+/**More Complex Styled app version  7 - 'MoreComplexTitle' 
  * Component - Features:
  * 
- *        --> Building a JSX 'h1' element and applying a 
- *            Style class 'title'.
+ *        -->Applying a 'box' class style defined on 'Wrapper'
+ *          Class Component.      
  * 
- *        --> Overwriting Style class 'title' by applying it
- *            on 'Wrapper' Component.
- * 
- *        --> Adding a Style class 'bg-redwine-shade' to 
- *            'Wrapper' Component      
- * 
- * Note: The 'Global Styles' topic is about a style that belong
- * to the Global Style sheet -this case index.css -, this case
- * i have a Style class 'title' that bring me all the styles and
- * applied it to an JSX 'h1' and in order to modify that 'Global
- * Styles' i have to define that same Style class 'title' on my
- * 'Wrapper' Component and modify it
+ * Note: the 'box' style class use a 'border' prop defined as 
+ * 'Global Variable' ( using first approach targeting ':root' 
+ * element )
  */
 
 const MoreComplexTitle = ({ title }) => {
@@ -31,6 +22,7 @@ const MoreComplexTitle = ({ title }) => {
             <h1>{title}</h1>
             <div className="underline">
             </div>
+            <div className="box" />
             <h2 className="title">Be the best</h2>
         </Wrapper>
         <RandomUnderline />
