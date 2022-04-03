0fcd8c47d013e6fe3035d9fc663d558f45a460cb
Erick Rodriguez <erickrod54@gmail.com>
2022-04-03 15:28:06 -0400

    Card app version 1 - App js file - Features: Setting up Card Component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react'
-import Setup from './setup-app/more-complex-title-app/morecomplextitle-app'
-import AlternativeTitle from './setup-app/more-complex-title-app/alternativetitle.component';
+import Setup from './setup-app/card-app/card.component'
 
-/**Style Components app version  8 - App js file - Features:
- *        --> Importing and Placing 'AlternativeTitle' Component
- *            with a 'title' prop of 'Alternative Title'
+/**Card app version  1 - 'App' js file - Features:
  * 
- * Note: This Component will be applied to set a 'Global Variable'
- * Style class using utils approach 
+ *        --> Setting up Card Component.
+ * 
+ * Note: Here i import the 'Card' Component
  */
 
 
 function App() {
   return (
     <>
-      <Setup title={'more complex title'}/>
-      <AlternativeTitle title={'Alternative Title'}/>
+      <Setup />
     </>
   );
 }
