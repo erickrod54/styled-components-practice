71b90c80107a98dd534bc59913cf07044e702bac
Erick Rodriguez <erickrod54@gmail.com>
2022-04-02 21:38:31 -0400

    More Complex Styled app version 8 - utils js - Features:(Alternative  approach to build Global Variables): Building a function setupBorder to set a Global Variable to keep and distribute Custom border prop

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,18 @@
-/**More Complex Styled app version  6 - 'utils js' - Features:
+/**More Complex Styled app version  8 - 'utils js' - Features:
  * (Alternative approach to build Global Variables)
  * 
- *        --> Building a 'Global Variable' to keep and distribute
- *            a color.
+ *        --> Building a function 'setupBorder' to set 
+ *            a'Global Variable' to keep and distribute
+ *            Custom border.
  * 
- * Note: this second approach dies the same that targeting ':root'
- * element, this time i'll use this 'utils' file to keep the 
- * styles
+ * Note: this second approach works even better than targeting 
+ * ':root' because here i can set a whole functionality that can
+ * carry with all css props from a class (this case i'll apply
+ * 'setupBroder' to 'box-alt')
+ * 
+ * I can also initializaed with default values as follows:
+ *      
+ *          ({width = 1, type = solid, color = purple}) 
  */
 
 /**this will be the Style component that will keep the 
@@ -15,4 +21,11 @@
 export const colors = {
     primary: '#e5d0ff'
 }
+
+/**here i set the function */
+export const setupBorder = ({width, type, color}) => {
+    /**i return a interpolated value*/
+    return `${width}px ${type} ${color}`
+}
+
 
