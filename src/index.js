5f6886970771cbc7d6302d6500778d8644974952
Erick Rodriguez <erickrod54@gmail.com>
2022-04-06 18:05:31 -0400

    Card app version 3 - index js file - Features: Comment index css file in order to test global-styles js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';
 import App from './App';
 
+/**Card app version  3 - index js file - Features:
+ * 
+ *        --> Comment 'index' css file in order to test
+ *            'global-styles' js
+ * 
+ * Note: This is an alternative to the 'Global Styles' sheet -
+ * index.css - and this alternative made use of Styled 
+ * Components.
+ * 
+ * --this implementation can be done for some cases where 
+ * --i cannot use the index css sheet, so this will suit well
+ */
 
 ReactDOM.render(
   <React.StrictMode>
