250fa08b8de0e499d3cb6bce3a847bd2ed632ea4
Erick Rodriguez <erickrod54@gmail.com>
2022-04-06 18:07:07 -0400

    Card app version 3 - App js file  - Features: Importing and placing GlobalStyles Component to set the global styles rules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import Setup from './setup-app/card-app/card.component'
+import React from 'react';
+import Setup from './setup-app/card-app/card.component';
+import GlobalStyles from './global-styles';
 
-/**Card app version  1 - 'App' js file - Features:
+/**Card app version  3 - 'App' js file - Features:
  * 
- *        --> Setting up Card Component.
+ *        --> Importing and placing 'GolbalStyles' component.
  * 
  * Note: Here i import the 'Card' Component
  */
@@ -12,6 +13,9 @@ import Setup from './setup-app/card-app/card.component'
 function App() {
   return (
     <>
+      {/**Here i place it and the 'Global Styles' are 
+       * applied */}
+      <GlobalStyles />
       <Setup />
     </>
   );
