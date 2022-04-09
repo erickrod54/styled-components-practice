0b3e4d90cd60a1375a02adf6d5448e07b4ac4751
Erick Rodriguez <erickrod54@gmail.com>
2022-04-08 22:18:10 -0400

    DarkTheme app version 1 - data js file - Features: Setting up data to use on DarkTheme app

---
+++ src/setup-app/dark-theme-app/data.js
@@ -0,0 +1,21 @@
+/**DarkTheme app version  1 - 'data' js file - 
+ * Features:
+ * 
+ *        --> Setting up data to use on 'DarkTheme' app.
+ * 
+ * Note: this app is about implementing Theming from 
+ * 'styled-components' library, speifically 'ThemeProvider' 
+ * Component in order to build a 'DarkTheme' app
+ *
+ * 'BaseTheme' ( Lighter ) is the oposite values
+ *  to 'DarkTheme' ( Darker )
+ */
+export const BaseTheme = {
+    color: '#222',
+    background: '#fff'
+  }
+  
+export const DarkTheme = {
+    color: '#fff',
+    background: '#222'
+  }
