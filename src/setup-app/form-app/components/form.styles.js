4eb3442c83e2c436bb19fe217dcd180208872972
Erick Rodriguez <erickrod54@gmail.com>
2022-04-28 16:32:04 -0400

    Form-app version 2 - form.styles file - Features: Building css helper function to set styles to submit button, Building BasicInput, Implementing attrs to handle type and placeholder for BasicInput

--- src/setup-app/form-app/components/form.styles.js
+++ src/setup-app/form-app/components/form.styles.js
@@ -1,20 +1,36 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-/**Form-app version 1 - 'form.styles' file - Features:
+/**Form-app version 2 - 'form.styles' file - Features:
  * 
- *          -->Building 'FormButton'.
+ *          -->Building 'css helper function' to set
+ *              styles to 'submit' button
  * 
- *          --> Adding 'attrs' to handle a default type.
+ *          -->Building 'BasicInput'.
  * 
- * Note: 'FormButton' has applied 'attrs' feature that is
- * as native handler for props, this case it handles for
- * a demonstration props as 'type':
+ *          -->Implementing 'attrs' to handle 'type'
+ *             and 'placeholder' for 'BasicInput'.
  * 
- *          --> 'click me' - type - 'text'
- * 
- *          --> 'submit' - type - 'submit'
- *   
+ * Note: as i use 'attrs' to bring the 'props.type' i can 
+ * also apply a 'css helper function' to conditionally set
+ * styles to a submit type
 */
+
+
+export const BasicInput = styled.input.attrs((props) => {
+    return{ 
+        /**here i set condition to handle type and placeholder */
+        type: props.type || 'text',
+        placeholder: props.placeholder || 'please enter value'
+    }
+})`
+    box-sizing: border-box;
+    padding: 0.5rem;
+    border: 2px solid #f2f4f8;
+    border-radius: 0.25rem;
+    width: 100%;
+    margin-top: 1rem;
+`
+
 
 export const FormButton = styled.button.attrs((props) => {
     /**this is 'props.type' exists, if not default value 'button'
@@ -27,4 +43,12 @@ export const FormButton = styled.button.attrs((props) => {
     color: white;
     padding: 0.25rem;
     cursor: pointer;
+    ${({type}) => {
+        return type === 'submit' && css`
+        display: block;
+        width: 100%;
+        margin-top:1rem;
+        border-radius: 0.25rem;
+        `
+    }}
 `
