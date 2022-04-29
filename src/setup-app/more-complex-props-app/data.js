0ead309c2016e269f04580f3b4fe6568c0621188
Erick Rodriguez <erickrod54@gmail.com>
2022-04-29 17:11:06 -0400

    MoreComplexPropsApp version 1 - more-complex-props-app file - Features: Building fruits array

---
+++ src/setup-app/more-complex-props-app/data.js
@@ -0,0 +1,12 @@
+
+/**MoreComplexPropsApp version 1 - 'more-complex-props-app' 
+ * file - Features:
+ * 
+ *        --> Building 'fruits' array.
+ * 
+ * Notes: 'fruits' is the data that is going to be use as data
+ * for the app.
+ * 
+ * */
+
+export const fruits = ['orange', 'apple', 'banana', 'peach']
