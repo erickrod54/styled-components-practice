92fbe56b902ec67d5df729efd1217d94f3edd2e4
Erick Rodriguez <erickrod54@gmail.com>
2022-04-30 19:40:32 -0400

    MoreComplexPropsApp version 2 - data file - Features: Building products array

--- src/setup-app/more-complex-props-app/data.js
+++ src/setup-app/more-complex-props-app/data.js
@@ -1,12 +1,29 @@
 
-/**MoreComplexPropsApp version 1 - 'more-complex-props-app' 
- * file - Features:
+/**MoreComplexPropsApp version 2 - 'data' file - Features:
  * 
- *        --> Building 'fruits' array.
+ *        --> Building 'products' array.
  * 
- * Notes: 'fruits' is the data that is going to be use as data
- * for the app.
+ * Notes: 'products' is the data that is going to be use to
+ * display a sample list of products.
  * 
  * */
 
 export const fruits = ['orange', 'apple', 'banana', 'peach']
+
+export const products = [
+    {
+        id: 1,
+        name: 'Bifamiliar Home',
+        price: 200         
+    },
+    {
+        id:2,
+        name: 'Single Home',
+        price: 300
+    },
+    {
+        id:3,
+        name: 'Multifamiliar Home',
+        price: 400
+    }
+]
